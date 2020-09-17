e69a3adddaa10184567f72768accc0530ff59c5a
Pradip Poudel <prp_slayer@hotmail.com>
2020-09-17 13:21:52 +1000

    Chat function

---
+++ app/Http/Controllers/ChatsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Message;
+use App\Events\MessageSent;
+
+class ChatsController extends Controller
+{
+
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    //Show chats
+
+    public function index(){
+        return view('chat');
+    }
+
+    //Fetch Messages
+
+    public function fetchMessage(){
+        return Message::with('user')->get();
+    }
+
+    //Send and Save message
+
+    public function sendMessage(Request $request) {
+        $user = Auth::user();
+
+        $message = $user->messages()->create([
+            'message' => $request->input('message')
+        ]);
+
+        broadcast(new MessageSent($user, $message))->toOthers();
+
+        return ['status' => 'Sent'];
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/chat',[ChatsController::class, 'index']);
+Route::get('/messages', [ChatsController::class, 'fetchMessage']);
+Route::post('/messages', [ChatsController::class, 'sendMessage']);
