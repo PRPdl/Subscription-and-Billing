d44cb02abdcb4afd90e8dd29d869fe5d47a3037b
Pradip Poudel <prp_slayer@hotmail.com>
2020-10-09 13:26:55 +1100

    User List added - Vue

---
+++ resources/js/components/UserList.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul class="user-list">
+        <li v-for="user in users"> 
+            <strong v-if="user.id == auth_user.id"> {{ user.name }} </strong>
+            <span v-else> {{ user.name }} </span>
+        </li>
+    </ul>
+
+</template>
+
+<script>
+    export default {
+        props: ['users', 'auth_user'],
+
+        created() {
+            console.log(this.users)
+        }
+    }
+
+</script>
